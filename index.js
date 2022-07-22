e4b4da6369f4d3b1686402cacd3fd172656d7146
fabiano.justino <fibianojustino@gmail.com>
2022-07-22 16:09:32 -0300

    fix: eslint arquivo index.js

--- index.js
+++ index.js
@@ -4,15 +4,11 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)=> {
-  res.send(`<h1> Executando na port ${port} </h1>`)
+app.get('/', (req, res) => {
+  res.send(`<h1> Executando na port ${port} </h1>`);
 });
 
 // listen
-app.listen(port, ()=> {
-console.log('rodando');
-});
-
+app.listen(port, () => console.log('rodando'));
